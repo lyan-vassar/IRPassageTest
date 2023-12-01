422170275bea7907eb7ee7a3b20b7e397d530ca8
LilyYan2001 <lyan@stuy.edu>
2023-12-01 10:31:22 -0500

    initial commit

---
+++ experiment.js
@@ -0,0 +1,120 @@
+// Loads jsPsych
+var jsPsych = initJsPsych({});
+
+// initial variables
+var animals = ["wombats", "jellyfish", "swallows", "anoles"]; 
+var order = jsPsych.randomization.repeat([0, 1, 2, 3], 1);
+var prompt = "Write down what you learned about ";
+var currentSentence = "";
+
+// Timeline that holds javascript variables (instructioins, stimuli) to appear in chronological order 
+var timeline = [
+];
+
+// unique id
+const subject_id = jsPsych.randomization.randomID(10);
+const filename = `${subject_id}.csv`;
+
+/* RUNNING THE EXPERIMENT */
+// Instructions
+var instructions = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: "<p>You will be presented with four passages. After every passage, you will be asked to write all that you've learned from it. You will not be able to refer back to the passages.</p><p>Press the 'b' key to begin.</p>",
+    choices: ["b"],
+};
+
+timeline.push(instructions);
+
+// trial 1
+var passage_1 = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: passages[order[0]],
+    choices: ["I'm done reading, and am ready to move on"]
+}
+
+currentSentence = prompt + animals[order[0]] + ".";
+
+var question_1 = {
+    type: jsPsychSurveyText,
+    questions: [
+        {prompt: currentSentence, rows: 8}
+    ]
+}
+
+timeline.push(passage_1, question_1);
+
+// trial 2
+var passage_2 = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: passages[order[1]],
+    choices: ["I'm done reading, and am ready to move on"]
+}
+
+currentSentence = prompt + animals[order[1]] + ".";
+
+var question_2 = {
+    type: jsPsychSurveyText,
+    questions: [
+        {prompt: currentSentence, rows: 8}
+    ]
+}
+
+timeline.push(passage_2, question_2);
+
+// trial 3
+var passage_3 = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: passages[order[2]],
+    choices: ["I'm done reading, and am ready to move on"]
+}
+
+currentSentence = prompt + animals[order[2]] + ".";
+
+var question_3 = {
+    type: jsPsychSurveyText,
+    questions: [
+        {prompt: currentSentence, rows: 8}
+    ]
+}
+
+timeline.push(passage_3, question_3);
+
+// trial 4
+var passage_4 = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: passages[order[3]],
+    choices: ["I'm done reading, and am ready to move on"]
+}
+
+currentSentence = prompt + animals[order[3]] + ".";
+
+var question_4 = {
+    type: jsPsychSurveyText,
+    questions: [
+        {prompt: currentSentence, rows: 8}
+    ]
+}
+
+timeline.push(passage_4, question_4);
+
+// datapipe stuff
+const save_data = {
+    type: jsPsychPipe,
+    action: "save",
+    experiment_id: "98vGvEoRFD20",
+    filename: filename,
+    data_string: ()=>jsPsych.data.get().csv()
+  };
+
+  timeline.push(save_data);
+
+
+//exit message
+var goodbye = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: "Thanks for participating! You can close the window now."
+}
+
+timeline.push(goodbye);
+
+jsPsych.run(timeline);
